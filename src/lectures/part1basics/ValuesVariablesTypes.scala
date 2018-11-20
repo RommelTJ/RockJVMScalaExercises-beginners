44b2c69adf0b23ff7cdf54c0091d89de9413a0c3
Rommel Rico <me@rommelrico.com>
2018-11-19 21:08:17 -0800

    Adding note that vals are immutable

--- src/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -2,8 +2,10 @@ package lectures.part1basics
 
 object ValuesVariablesTypes extends App {
 
-  // Syntax for declaring a val.
+  // Syntax for declaring a val. Vals are immutable.
   val x: Int = 42
   println(x)
+
   
+
 }
