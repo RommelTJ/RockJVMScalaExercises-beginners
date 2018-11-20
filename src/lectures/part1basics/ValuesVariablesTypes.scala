805fea2e099de741cf3bbc7349f3d7d0befc6808
Rommel Rico <me@rommelrico.com>
2018-11-19 21:08:59 -0800

    Types of vals are inferred, so you don't have to type 42.

--- src/lectures/part1basics/ValuesVariablesTypes.scala
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -3,9 +3,10 @@ package lectures.part1basics
 object ValuesVariablesTypes extends App {
 
   // Syntax for declaring a val. Vals are immutable.
-  val x: Int = 42
+  // Types of vals are inferred, so you don't have to type 42.
+  val x = 42
   println(x)
 
-  
+
 
 }
