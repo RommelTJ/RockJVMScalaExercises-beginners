118b141f3018163b7667e578eb28d8df8ca8db97
Rommel Rico <me@rommelrico.com>
2018-11-21 09:16:16 -0800

    Is Prime function with auxiliary function

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -52,4 +52,15 @@ object Functions extends App {
   }
   println(aFibonacci(8))
 
+  // 4. Testing if a number is Prime.
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean = {
+      if (t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+    }
+
+    isPrimeUntil(n/2)
+  }
+  println(isPrime(7))
+
 }
