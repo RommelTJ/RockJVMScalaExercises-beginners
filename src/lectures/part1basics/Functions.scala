d531f79cca85738158dafbb67d585bdc9e665b4c
Rommel Rico <me@rommelrico.com>
2018-11-21 08:15:59 -0800

    Function with code block.

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -4,5 +4,9 @@ object Functions extends App {
 
   // Scala Functions.
   def aFunction(a: String, b: Int): String = a + " " + b
+  // This is a function with a code block, which is also an expression in Scala. 
+  def aFunction2(a: String, b: Int): String = {
+    a + " " + b
+  }
 
 }
