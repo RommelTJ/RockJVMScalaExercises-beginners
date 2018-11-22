5086424278229ea2da4753838fe31f771ab7254f
Rommel Rico <me@rommelrico.com>
2018-11-22 09:27:45 -0800

    Labeled and default arguments

--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -12,4 +12,14 @@ object DefaultArgs extends App {
   val fact11 = tailRecFactorial(11)
   println(fact11)
 
+  // Dummy Example to show default values that should be omitted should be at the end or you should name the arguments.
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("Saving Picture")
+  savePicture("jpg", 800, 600)
+  savePicture(height = 600, width = 800) // This works, even if out of order, because they're labeled.
+  // savePicture(800, 600) // This doesn't work.
+  // savePicture(800) // This doesn't work.
+  savePicture(width = 800) // This works.
+  // Pass in every leading argument, or name the arguments.
+
+
 }
