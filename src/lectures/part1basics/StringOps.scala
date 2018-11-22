4a70a59f065a408dc1b95e5a7dba023ef283b6e3
Rommel Rico <me@rommelrico.com>
2018-11-22 09:31:22 -0800

    charAt

--- src/lectures/part1basics/StringOps.scala
+++ src/lectures/part1basics/StringOps.scala
@@ -4,4 +4,7 @@ object StringOps extends App {
 
   val str: String = "Hello, I am learning Scala"
 
+  // Sample String Operations.
+  println(str.charAt(2)) // Returns character at index 2, 'l'.
+
 }
