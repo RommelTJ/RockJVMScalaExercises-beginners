e5b1a11f17b6c5de579a84ea81e36655722c769e
Rommel Rico <me@rommelrico.com>
2018-11-22 09:22:53 -0800

    Using default values

--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -2,10 +2,14 @@ package lectures.part1basics
 
 object DefaultArgs extends App {
 
-  def tailRecFactorial(n: Int, acc: Int): Int =
+  // Using a default value.
+  def tailRecFactorial(n: Int, acc: Int = 1): Int =
     if (n <= 1) acc
     else tailRecFactorial(n - 1, n * acc)
 
   val fact10 = tailRecFactorial(10, 1)
   println(fact10)
+  val fact11 = tailRecFactorial(11)
+  println(fact11)
+
 }
