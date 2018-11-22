a717ddb01db50a6cad3c8bf10e9ee8aebb7a2119
Rommel Rico <me@rommelrico.com>
2018-11-22 09:11:02 -0800

    Examples

--- src/lectures/part1basics/CBNvsCBV.scala
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -9,8 +9,11 @@ object CBNvsCBV extends App {
 
   // The '=>' parameter tells the compiler that the parameter will be called by name.
   def calledByName(x: => Long): Unit = {
-    println("By value: " + x)
-    println("By value: " + x)
+    println("By name: " + x)
+    println("By name: " + x)
   }
+
+  calledByValue(System.nanoTime()) // This returns the same time twice.
+  calledByName(System.nanoTime()) // This returns two different times.
 
 }
