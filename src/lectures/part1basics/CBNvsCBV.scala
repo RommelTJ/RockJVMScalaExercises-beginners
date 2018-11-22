8c48e948e5a4a6746a024b11058c938aec677314
Rommel Rico <me@rommelrico.com>
2018-11-22 09:16:29 -0800

    A dummy infinite function demo

--- src/lectures/part1basics/CBNvsCBV.scala
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -20,4 +20,8 @@ object CBNvsCBV extends App {
   // In By Value, the exact value is computed before the function evaluates.
   // In By Name, the expression is passed lirerally as-is and the expression is evaluated every time.
 
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+  printFirst(34, infinite())
+
 }
