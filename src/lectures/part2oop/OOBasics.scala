091bafd05fad44c51ea55692e595511489b93bb9
Rommel Rico <me@rommelrico.com>
2018-11-23 17:03:52 -0800

    Adding a body to the Person class

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -5,9 +5,14 @@ object OOBasics extends App {
   // Instantiating a class in Scala
   val person = new Person("Rommel", 30)
   println(person.age)
+  println(person.x)
 
 }
 
 // Class with constructor
 // Class parameters are not fields in Scala. Add "val" or "var" to make them also fields.
-class Person(val name: String, val age: Int)
+class Person(val name: String, val age: Int) {
+  // Body
+  val x = 2
+  println(1 + 3)
+}
