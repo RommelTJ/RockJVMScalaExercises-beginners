dd9ddd3205e14852f1b53a8cb19dca1bbd6eb62e
Rommel Rico <me@rommelrico.com>
2018-11-23 18:24:24 -0800

    Solution to 3

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -5,6 +5,8 @@ object MethodNotations extends App {
   class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}."
+    def learns(topic: String): String = s"$name learns $topic."
+    def learnsScala: String = learns("Scala")
     def +(person: Person): String = s"${person.name} is hanging out with ${this.name}."
     def +(nickname: String): String = s"$name ($nickname)."
     def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
@@ -50,6 +52,8 @@ object MethodNotations extends App {
 
   // 3. Add "learns" method in Person class that takes String => "Mary learns $leans". Then add learnsScala method
   // that calls learns method with "Scala". Use it in postfix notation.
+  println(mary.learns("Boogers"))
+  println(mary learnsScala)
 
   // 4. Overload apply method to receive a number and return String
   // mary.apply(2) => "Mary watched Inception 2 times."
