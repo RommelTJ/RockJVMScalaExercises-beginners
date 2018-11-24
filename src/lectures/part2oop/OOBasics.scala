b294f2a2808289dc0718fcbe5316ef0996a05b8e
Rommel Rico <me@rommelrico.com>
2018-11-23 17:11:26 -0800

    Overloading/Multiple constructors

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -13,10 +13,15 @@ object OOBasics extends App {
 
 // Class with constructor
 // Class parameters are not fields in Scala. Add "val" or "var" to make them also fields.
-class Person(name: String, val age: Int) {
+class Person(name: String, val age: Int = 0) {
   // Body
   val x = 2
   println(1 + 3)
+
+  // Overloading/Multiple constructors.
+  def this(name: String) = this(name, 0) // Calls primary constructor with an auxiliary constructor.
+  def this() = this("John Doe")
+
 
   // Implied parameter.
   def greet(): Unit = println(s"Hi, I am $name")
