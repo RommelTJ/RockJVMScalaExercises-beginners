984edbbad925c974a71ffb6d4b075106009f04d8
Rommel Rico <me@rommelrico.com>
2018-11-23 18:01:45 -0800

    "operators" in Scala

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -4,11 +4,16 @@ object MethodNotations extends App {
 
   class Person(val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
+    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}."
   }
 
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception")) // prints true
   // Infix notation or Operator Notation. Only works with methods with only one parameter.
   println(mary likes "Inception") // Also prints true!
+
+  // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary hangOutWith tom)
 
 }
