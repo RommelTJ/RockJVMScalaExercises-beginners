483d88790b7dd3263207e757bd8a36b2aa043a25
Rommel Rico <me@rommelrico.com>
2018-11-23 18:03:19 -0800

    Permissive method naming

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -5,6 +5,7 @@ object MethodNotations extends App {
   class Person(val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}."
+    def +(person: Person): String = s"${person.name} is hanging out with ${this.name}."
   }
 
   val mary = new Person("Mary", "Inception")
@@ -15,5 +16,6 @@ object MethodNotations extends App {
   // "operators" in Scala
   val tom = new Person("Tom", "Fight Club")
   println(mary hangOutWith tom)
+  println(mary + tom) // Scala is very permissive with method naming!
 
 }
