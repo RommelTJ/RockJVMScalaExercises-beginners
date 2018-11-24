f74a3a6ce1b811c2d047945f68856cba97599583
Rommel Rico <me@rommelrico.com>
2018-11-23 17:01:48 -0800

    Making class parameters as fields

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -4,10 +4,10 @@ object OOBasics extends App {
 
   // Instantiating a class in Scala
   val person = new Person("Rommel", 30)
-  println(person)
+  println(person.age)
 
 }
 
 // Class with constructor
-// Class parameters are not fields in Scala.
-class Person(name: String, age: Int)
+// Class parameters are not fields in Scala. Add "val" or "var" to make them also fields.
+class Person(val name: String, val age: Int)
