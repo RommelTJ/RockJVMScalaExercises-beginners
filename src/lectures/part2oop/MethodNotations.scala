36c2efd44aad35103f50de566c3dc6860a1a8c1f
Rommel Rico <me@rommelrico.com>
2018-11-23 17:56:40 -0800

    Adding a simple Person class to the MethodNotations object

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,6 +2,8 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  
+  class Person(val name: String, favoriteMovie: String) {
+    def likes(movie: String): Boolean = movie == favoriteMovie
+  }
 
 }
