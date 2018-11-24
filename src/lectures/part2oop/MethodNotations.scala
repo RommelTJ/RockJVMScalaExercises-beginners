6185b1460b358abad40fe5ff4e61d1f0cb311461
Rommel Rico <me@rommelrico.com>
2018-11-23 17:58:58 -0800

    Infix notation or Operator

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -8,5 +8,7 @@ object MethodNotations extends App {
 
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception")) // prints true
+  // Infix notation or Operator Notation. Only works with methods with only one parameter.
+  println(mary likes "Inception") // Also prints true!
 
 }
