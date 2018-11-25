a775afec2d0861b83201ab62b90c8aab08726844
Rommel Rico <me@rommelrico.com>
2018-11-24 20:05:34 -0800

    Also works with methods

--- src/lectures/part2oop/Objects.scala
+++ src/lectures/part2oop/Objects.scala
@@ -5,8 +5,10 @@ object Objects extends App {
   // SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY ("static")
   object Person {
     val N_EYES = 2
+    def canFly: Boolean = false
   }
 
   println(Person.N_EYES)
+  println(Person.canFly)
 
 }
