eb330eb52cffabf75c2544e3bce14313cf01208f
Rommel Rico <me@rommelrico.com>
2018-11-25 12:45:50 -0800

    Implementing toString

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -14,7 +14,9 @@ abstract class MyList {
   def isEmpty: Boolean
   def add(element: Int): MyList
 
-  override def toString: String = super.toString
+  override def toString: String = s"[$printElements]"
+
+  def printElements: String
 
 }
 
@@ -23,6 +25,9 @@ object Empty extends MyList {
   override def tail: MyList = throw new NoSuchElementException
   override def isEmpty: Boolean = true
   override def add(element: Int): MyList = new Cons(element, Empty)
+
+  def printElements: String = ""
+
 }
 
 class Cons(h: Int, t: MyList) extends MyList {
@@ -30,6 +35,12 @@ class Cons(h: Int, t: MyList) extends MyList {
   override def tail: MyList = t
   override def isEmpty: Boolean = false
   override def add(element: Int): MyList = new Cons(element, this)
+
+  def printElements: String = {
+    if (t.isEmpty) "" + h
+    else s"$h ${t.printElements}"
+  }
+
 }
 
 object ListTest extends App {
@@ -37,4 +48,5 @@ object ListTest extends App {
   println(list.tail.head)
   println(list.add(4).head)
   println(list.isEmpty)
+  println(list.toString)
 }
