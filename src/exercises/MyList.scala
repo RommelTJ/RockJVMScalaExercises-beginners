835234568d9edb8542262d2297a0f50d3c4a2acb
Rommel Rico <me@rommelrico.com>
2018-11-25 12:23:41 -0800

    MyList notes

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,5 +1,12 @@
 package exercises
 
+/**
+  * head = first element of this list.
+  * tail = remainder of this list.
+  * isEmpty = is this list empty.
+  * add(int) => new list with this element added
+  * toString => a string representation of the list.
+  */
 abstract class MyList {
 
   
