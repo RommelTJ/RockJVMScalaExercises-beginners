32fc4a479f6eb7458a2f5bee5882864e49758925
Rommel Rico <me@rommelrico.com>
2018-11-25 12:13:46 -0800

    Using a trait

--- src/lectures/part2oop/AbstractDataTypes.scala
+++ src/lectures/part2oop/AbstractDataTypes.scala
@@ -21,7 +21,11 @@ object AbstractDataTypes extends App {
   class Crocodile extends Animal with Carnivore {
     override val creatureType: String = "Lizard"
     override def eat(): Unit = println("chomp chomp chomp")
-    def eat(animal: Animal): Unit = s"I'm a croc and I'm eating ${animal.creatureType}."
+    def eat(animal: Animal): Unit = println(s"I'm a croc and I'm eating ${animal.creatureType}.")
   }
+
+  val dog = new Dog
+  val croc = new Crocodile
+  croc.eat(dog)
 
 }
