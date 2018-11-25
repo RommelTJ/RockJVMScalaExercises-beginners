8cd21203065d58118d51a33aad898ce704ee113a
Rommel Rico <me@rommelrico.com>
2018-11-25 13:24:22 -0800

    Making MyList to be Generic

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -7,12 +7,12 @@ package exercises
   * add(int) => new list with this element added
   * toString => a string representation of the list.
   */
-abstract class MyList {
+abstract class MyList[+A] {
 
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
 
   // Polymorphic Method.
   override def toString: String = s"[$printElements]"
@@ -21,21 +21,21 @@ abstract class MyList {
 
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException
-  override def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(element: Int): MyList = new Cons(element, Empty)
+  override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   def printElements: String = ""
 
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
-  override def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
+  override def tail: MyList[A] = t
   override def isEmpty: Boolean = false
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   def printElements: String = {
     if (t.isEmpty) "" + h
@@ -45,9 +45,6 @@ class Cons(h: Int, t: MyList) extends MyList {
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.isEmpty)
-  println(list.toString)
+  val listOfIntegers: MyList[Int] = Empty
+  val listOfStrings: MyList[String] = Empty
 }
