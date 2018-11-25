2e9c818821f4844a4c58f73bc2f5ccf291b51cc1
Rommel Rico <me@rommelrico.com>
2018-11-25 12:07:56 -0800

    Abstract Animal class

--- src/lectures/part2oop/AbstractDataTypes.scala
+++ src/lectures/part2oop/AbstractDataTypes.scala
@@ -2,6 +2,10 @@ package lectures.part2oop
 
 object AbstractDataTypes extends App {
 
-  
+  // Abstract classes
+  abstract class Animal {
+    val creatureType: String
+    def eat(): Unit
+  }
 
 }
