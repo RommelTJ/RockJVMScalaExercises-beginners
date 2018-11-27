73f43a375df38825d372501d802aaaf8fc821b63
Rommel Rico <me@rommelrico.com>
2018-11-26 21:02:17 -0800

    Adding a filter method

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -21,6 +21,7 @@ abstract class MyList[+A] {
 
   def map[B](transformer: MyTransformer[A, B]): MyList[B]
   def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
 
 }
 
