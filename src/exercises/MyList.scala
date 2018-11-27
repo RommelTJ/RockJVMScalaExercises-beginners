01ae53b6b241c6f9fa5869af7d7a0b67d8addbee
Rommel Rico <me@rommelrico.com>
2018-11-26 21:03:21 -0800

    Implementing new methods in Empty object

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -33,6 +33,10 @@ object Empty extends MyList[Nothing] {
 
   def printElements: String = ""
 
+  override def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+  override def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  override def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -45,6 +49,12 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (t.isEmpty) "" + h
     else s"$h ${t.printElements}"
   }
+
+  override def map[B](transformer: MyTransformer[A, B]): MyList[B] = ???
+
+  override def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = ???
+
+  override def filter(predicate: MyPredicate[A]): MyList[A] = ???
 
 }
 
