b37b608ee64e407b94012f2dd3dec3b32a473e93
Rommel Rico <me@rommelrico.com>
2018-11-27 21:07:57 -0800

    Package members are accessible by their simple name.

--- src/lectures/part2oop/PackagingAndImports.scala
+++ src/lectures/part2oop/PackagingAndImports.scala
@@ -2,4 +2,7 @@ package lectures.part2oop
 
 object PackagingAndImports extends App {
 
+  // Package members are accessible by their simple name.
+  val write = new Writer("Daniel", "RockTheJVM", 2018)
+
 }
