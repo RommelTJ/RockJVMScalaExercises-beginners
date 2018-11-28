3af7158034e2b2b0f1e4f2454b8f50e8d39bbef2
Rommel Rico <me@rommelrico.com>
2018-11-27 20:31:41 -0800

    Throwing an exception

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -3,6 +3,13 @@ package lectures.part2oop
 object Exceptions extends App {
 
   val x: String = null
-  println(x.length)
+  // println(x.length) - NullPointerException
+
+  // throwing and catching exceptions.
+
+  val aWeirdValue: String = throw new NullPointerException // Nothing.
+
+  // throwable classes extend the Throwable class.
+  // Exception and Error are the major Throwable subtypes.
 
 }
