50c883460ff6b3dafe631caa26e876d74f861634
Rommel Rico <me@rommelrico.com>
2018-11-27 21:02:46 -0800

    Handling multiple exception

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -68,7 +68,14 @@ object Exceptions extends App {
       else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
       else result
     }
-    def multiply(x: Int, y: Int): Int = x * y
+    def multiply(x: Int, y: Int): Int = {
+      val result = x * y
+      if (x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result < 0) throw new OverflowException
+      else if (x > 0 && y < 0 && result > 0) throw new UnderflowException
+      else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
     def divide(x: Int, y: Int): Int = x / y
   }
 
