a4ab14fe94f717169cd8a7c05a1b00b656a7f6cf
Rommel Rico <me@rommelrico.com>
2018-11-27 20:12:29 -0800

    Class parameters are promoted to fields.

--- src/lectures/part2oop/CaseClasses.scala
+++ src/lectures/part2oop/CaseClasses.scala
@@ -5,6 +5,10 @@ object CaseClasses extends App {
   /*
   equals, hashCode, toString.
    */
-  case class Person(name: String, age: Int) 
+  case class Person(name: String, age: Int)
+
+  // 1 - Class parameters are promoted to fields.
+  val jim = new Person("Jim", 34)
+  println(jim.name)
 
 }
