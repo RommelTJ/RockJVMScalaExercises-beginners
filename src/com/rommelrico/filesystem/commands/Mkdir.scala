157af74dbe5436f9e65a5e58e9b02ced8b2c85e8
Rommel Rico <me@rommelrico.com>
2018-11-28 21:31:21 -0800

    Implementing the apply method for Mkdir

--- src/com/rommelrico/filesystem/commands/Mkdir.scala
+++ src/com/rommelrico/filesystem/commands/Mkdir.scala
@@ -1,6 +1,26 @@
 package com.rommelrico.filesystem.commands
 import com.rommelrico.filesystem.State
+import com.rommelrico.filesystem.files.Directory
 
 class Mkdir(name: String) extends Command {
-  override def apply(state: State): State = ???
+
+  override def apply(state: State): State = {
+    val wd = state.wd
+    if (wd.hasEntry(name)) {
+      state.setMessage(s"Entry $name already exists!")
+    } else if (name.contains(Directory.SEPARATOR)) {
+      state.setMessage(s"$name must not contain separators!")
+    } else if (checkIllegal(name)) {
+      state.setMessage(s"$name: illegal entry name!")
+    } else {
+      doMkdir(name, state)
+    }
+  }
+
+  def checkIllegal(name: String): Boolean = {
+    name.contains(".")
+  }
+
+  def doMkdir(name: String, state: State): State = ???
+
 }
