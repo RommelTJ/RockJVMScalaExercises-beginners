5d2611e459132926572f77699d10c68d0879fdd6
Rommel Rico <me@rommelrico.com>
2018-11-28 21:23:06 -0800

    Handling empty strings

--- src/com/rommelrico/filesystem/commands/Command.scala
+++ src/com/rommelrico/filesystem/commands/Command.scala
@@ -16,7 +16,7 @@ object Command {
 
   def from(input: String): Command = {
     val tokens: Array[String] = input.split(" ")
-    if (tokens.isEmpty) emptyCommand
+    if (input.isEmpty || tokens.isEmpty) emptyCommand
     else if (MKDIR.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(MKDIR)
       else new Mkdir(tokens(1))
