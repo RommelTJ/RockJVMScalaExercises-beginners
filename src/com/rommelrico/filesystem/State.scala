b16a138ceb24fa1b2880672d22fe2e5746f8ed71
Rommel Rico <me@rommelrico.com>
2018-11-28 21:08:56 -0800

    Small adjustment

--- src/com/rommelrico/filesystem/State.scala
+++ src/com/rommelrico/filesystem/State.scala
@@ -4,7 +4,7 @@ import com.rommelrico.filesystem.files.Directory
 
 class State(val root: Directory, val wd: Directory, val output: String) {
   def show(): Unit = {
-    print(output)
+    println(output)
     print(State.SHELL_TOKEN)
   }
   def setMessage(message: String): State = State.apply(root, wd, message)
