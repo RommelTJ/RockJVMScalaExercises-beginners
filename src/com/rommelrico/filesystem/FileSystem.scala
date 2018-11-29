f83c0ac5fdfa3f05068d7d49c2b8f646683dfb11
Rommel Rico <me@rommelrico.com>
2018-11-28 21:07:27 -0800

    Adding root and state to the FileSystem and testing the Unknown command

--- src/com/rommelrico/filesystem/FileSystem.scala
+++ src/com/rommelrico/filesystem/FileSystem.scala
@@ -2,13 +2,19 @@ package com.rommelrico.filesystem
 
 import java.util.Scanner
 
+import com.rommelrico.filesystem.commands.Command
+import com.rommelrico.filesystem.files.Directory
+
 object FileSystem extends App {
 
+  val root = Directory.ROOT
+  var state = State(root, root)
   val scanner = new Scanner(System.in)
 
   while(true) {
-    print("$")
-    println(scanner.nextLine())
+    state.show()
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
   }
 
 }
