03ef22baa23e551b878922af668be18484e78475
Rommel Rico <me@rommelrico.com>
2018-11-28 20:57:39 -0800

    Adding methods to the State class

--- src/com/rommelrico/filesystem/State.scala
+++ src/com/rommelrico/filesystem/State.scala
@@ -3,9 +3,12 @@ package com.rommelrico.filesystem
 import com.rommelrico.filesystem.files.Directory
 
 class State(val root: Directory, val wd: Directory, val output: String) {
-  
+  def show(): Unit = print(State.SHELL_TOKEN)
 }
 
 object State {
   val SHELL_TOKEN = "$ "
+
+  def apply(root: Directory, wd: Directory, output: String = ""): State = new State(root, wd, output)
+  
 }
