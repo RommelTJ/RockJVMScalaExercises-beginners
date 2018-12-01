6f0b142aefb53f6d0e8cd7de133d10af035d87e2
Rommel Rico <me@rommelrico.com>
2018-11-30 22:07:52 -0800

    Getting the absolutePath

--- src/com/rommelrico/filesystem/commands/Cd.scala
+++ src/com/rommelrico/filesystem/commands/Cd.scala
@@ -1,13 +1,20 @@
 package com.rommelrico.filesystem.commands
 import com.rommelrico.filesystem.State
+import com.rommelrico.filesystem.files.Directory
 
 class Cd(dir: String) extends Command {
 
   override def apply(state: State): State = {
     // 1 - Find Root
     val root = state.root
+    val wd = state.wd
 
     // 2 - Find Absolute Path of the directory I want to CD to.
+    val absolutePath = {
+      if (dir.startsWith(Directory.SEPARATOR)) dir
+      else if (wd.isRoot) wd.path + dir
+      else wd.path + Directory.SEPARATOR + dir
+    }
 
     // 3 - Find the directory to CD to given the path.
 
