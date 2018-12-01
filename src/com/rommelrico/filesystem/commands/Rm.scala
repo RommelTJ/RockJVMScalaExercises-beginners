5615b70c6ba6ce48193820fe754c3f69440ff3b5
Rommel Rico <me@rommelrico.com>
2018-12-01 11:49:44 -0800

    Implementing the apply method in Rm

--- src/com/rommelrico/filesystem/commands/Rm.scala
+++ src/com/rommelrico/filesystem/commands/Rm.scala
@@ -1,6 +1,25 @@
 package com.rommelrico.filesystem.commands
 import com.rommelrico.filesystem.State
+import com.rommelrico.filesystem.files.Directory
 
 class Rm(name: String) extends Command {
-  override def apply(state: State): State = ???
+
+  override def apply(state: State): State = {
+    // 1 - Get working directory.
+    val wd = state.wd
+
+    // 2 - Get Absolute Path.
+    val absolutePath = {
+      if (name.startsWith(Directory.SEPARATOR)) name
+      else if (wd.isRoot) wd.path + name
+      else wd.path + Directory.SEPARATOR + name
+    }
+
+    // 3 - Do Some checks.
+    if (Directory.ROOT_PATH.equals(absolutePath)) state.setMessage("Nuclear war not supported yet!")
+    else doRm(state, absolutePath)
+  }
+
+  def doRm(state: State, path: String): State = ???
+
 }
