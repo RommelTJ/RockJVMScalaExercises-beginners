f48039eead10f28903caf475cd18f8c6d3ff8ad2
Rommel Rico <me@rommelrico.com>
2018-11-30 22:04:53 -0800

    Apply steps

--- src/com/rommelrico/filesystem/commands/Cd.scala
+++ src/com/rommelrico/filesystem/commands/Cd.scala
@@ -2,5 +2,16 @@ package com.rommelrico.filesystem.commands
 import com.rommelrico.filesystem.State
 
 class Cd(dir: String) extends Command {
-  override def apply(state: State): State = ???
+
+  override def apply(state: State): State = {
+    // 1 - Find Root
+
+    // 2 - Find Absolute Path of the directory I want to CD to.
+
+    // 3 - Find the directory to CD to given the path.
+
+    // 4 - Change the state given the new directory.
+
+  }
+
 }
