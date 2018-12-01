56454266c8852ed8b8983e8229f1ca90f0a11e4d
Rommel Rico <me@rommelrico.com>
2018-12-01 13:07:28 -0800

    Fixing the Command Trait

--- src/com/rommelrico/filesystem/commands/Command.scala
+++ src/com/rommelrico/filesystem/commands/Command.scala
@@ -2,9 +2,7 @@ package com.rommelrico.filesystem.commands
 
 import com.rommelrico.filesystem.State
 
-trait Command {
-  def apply(state: State): State
-}
+trait Command extends (State => State)
 
 object Command {
 
