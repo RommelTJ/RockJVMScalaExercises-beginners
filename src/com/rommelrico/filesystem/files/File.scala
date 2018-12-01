4655d07f6c6ba7116bb9f99eb88142fb6da7737f
Rommel Rico <me@rommelrico.com>
2018-11-30 21:43:52 -0800

    File companion object and empty file generation

--- src/com/rommelrico/filesystem/files/File.scala
+++ src/com/rommelrico/filesystem/files/File.scala
@@ -6,9 +6,13 @@ class File(override val parentPath: String, override val name: String, contents:
   extends DirEntry(parentPath, name) {
 
   override def asDirectory: Directory = throw new FileSystemException("A file cannot be converted to a Directory!")
-  
+
   override def asFile: File = this
 
   override def getType: String = "File"
 
+}
+
+object File {
+  def empty(parentPath: String, name: String): File = new File(parentPath, name, "")
 }
