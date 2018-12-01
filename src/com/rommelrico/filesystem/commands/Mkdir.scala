81cca04cf71fe76e5af1d354b4dcdeae3246cc09
Rommel Rico <me@rommelrico.com>
2018-11-30 21:49:28 -0800

    New Mkdir implementation

--- src/com/rommelrico/filesystem/commands/Mkdir.scala
+++ src/com/rommelrico/filesystem/commands/Mkdir.scala
@@ -1,7 +1,5 @@
 package com.rommelrico.filesystem.commands
-import com.rommelrico.filesystem.State
-import com.rommelrico.filesystem.files.{DirEntry, Directory}
 
-class Mkdir(name: String) extends Command {
+class Mkdir(name: String) extends CreateEntry(name) {
 
 }
