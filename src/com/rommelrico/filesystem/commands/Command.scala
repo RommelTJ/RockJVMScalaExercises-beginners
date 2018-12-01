61cffabae715a21f008c655129b968e9c72c02e6
Rommel Rico <me@rommelrico.com>
2018-11-30 22:23:57 -0800

    Adding CD to Commands

--- src/com/rommelrico/filesystem/commands/Command.scala
+++ src/com/rommelrico/filesystem/commands/Command.scala
@@ -12,6 +12,7 @@ object Command {
   val LS = "ls"
   val PWD = "pwd"
   val TOUCH = "touch"
+  val CD = "cd"
 
   def emptyCommand: Command = (state: State) => state
 
@@ -30,6 +31,9 @@ object Command {
     } else if (TOUCH.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(TOUCH)
       else new Touch(tokens(1))
+    } else if (CD.equals(tokens(1))) {
+      if (tokens.length < 2) incompleteCommand(CD)
+      else new Cd(tokens(1))
     } else new UnknownCommand
   }
 }
