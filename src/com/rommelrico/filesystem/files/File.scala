d29f5845138fd277ec527265897925a8263eb717
Rommel Rico <me@rommelrico.com>
2018-11-30 21:42:31 -0800

    Implementing asFile in File class

--- src/com/rommelrico/filesystem/files/File.scala
+++ src/com/rommelrico/filesystem/files/File.scala
@@ -6,6 +6,8 @@ class File(override val parentPath: String, override val name: String, contents:
   extends DirEntry(parentPath, name) {
 
   override def asDirectory: Directory = throw new FileSystemException("A file cannot be converted to a Directory!")
+  
+  override def asFile: File = this
 
   override def getType: String = "File"
 
