cdb55eac3aa801a8aa6dedb7b0e4e9911eba671e
Rommel Rico <me@rommelrico.com>
2018-12-01 13:13:36 -0800

    Removing the var in FileSystem

--- src/com/rommelrico/filesystem/FileSystem.scala
+++ src/com/rommelrico/filesystem/FileSystem.scala
@@ -1,20 +1,14 @@
 package com.rommelrico.filesystem
 
-import java.util.Scanner
-
 import com.rommelrico.filesystem.commands.Command
 import com.rommelrico.filesystem.files.Directory
 
 object FileSystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
-
-  while(true) {
-    state.show()
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show()
+    Command.from(newLine).apply(currentState)
+  })
 
 }
