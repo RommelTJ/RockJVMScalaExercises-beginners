9f66c7abaa24f7d2b03cf6235a2ed3e3e8a8d4ee
Rommel Rico <me@rommelrico.com>
2018-11-30 21:38:40 -0800

    File implementation

--- src/com/rommelrico/filesystem/files/File.scala
+++ src/com/rommelrico/filesystem/files/File.scala
@@ -3,8 +3,8 @@ package com.rommelrico.filesystem.files
 class File(override val parentPath: String, override val name: String, contents: String)
   extends DirEntry(parentPath, name) {
 
-  override def asDirectory: Directory = ???
+  override def asDirectory: Directory = throw new FileSystemException("A file cannot be converted to a Directory!")
 
-  override def getType: String = ???
+  override def getType: String = "File"
 
 }
