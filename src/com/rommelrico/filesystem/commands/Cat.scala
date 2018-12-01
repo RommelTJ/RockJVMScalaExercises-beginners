83ea273433fb22d9640b5d81997335e371b8aa22
Rommel Rico <me@rommelrico.com>
2018-12-01 12:54:41 -0800

    Cat Command

---
+++ src/com/rommelrico/filesystem/commands/Cat.scala
@@ -0,0 +1,6 @@
+package com.rommelrico.filesystem.commands
+import com.rommelrico.filesystem.State
+
+class Cat(filename: String) extends Command {
+  override def apply(state: State): State = ???
+}
