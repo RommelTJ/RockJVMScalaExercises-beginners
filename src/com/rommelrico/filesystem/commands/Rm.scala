237b1be68e5a34b1424438e6214dd4764c75736e
Rommel Rico <me@rommelrico.com>
2018-12-01 11:54:37 -0800

    doRm implementation

--- src/com/rommelrico/filesystem/commands/Rm.scala
+++ src/com/rommelrico/filesystem/commands/Rm.scala
@@ -20,6 +20,15 @@ class Rm(name: String) extends Command {
     else doRm(state, absolutePath)
   }
 
-  def doRm(state: State, path: String): State = ???
+  def doRm(state: State, path: String): State = {
+
+    def rmHelper(directory: Directory, str: String): Directory = ???
+
+    // 4 - Get or find the entry to remove and update structure like we do for mkdir.
+    val tokens = path.substring(1).split(Directory.SEPARATOR).toList
+    val newRoot: Directory = rmHelper(state.root, path)
+    if (newRoot == state.root) state.setMessage(s"$path: no such file or directory.")
+    else State(newRoot, newRoot.findDescendant(state.wd.path.substring(1)))
+  }
 
 }
