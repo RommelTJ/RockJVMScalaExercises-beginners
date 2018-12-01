3ca4b74d9fe90d764653d30aa7a400fa6762bbc7
Rommel Rico <me@rommelrico.com>
2018-12-01 12:15:40 -0800

    Echo Command

---
+++ src/com/rommelrico/filesystem/commands/Echo.scala
@@ -0,0 +1,6 @@
+package com.rommelrico.filesystem.commands
+import com.rommelrico.filesystem.State
+
+class Echo(args: Array[String]) extends Command {
+  override def apply(state: State): State = ???
+}
