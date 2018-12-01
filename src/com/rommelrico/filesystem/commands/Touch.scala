e66d8d3c9e806d800b6bbd1e231189817d7ce5ba
Rommel Rico <me@rommelrico.com>
2018-11-30 21:55:36 -0800

    Touch createSpecificEntry implementation

---
+++ src/com/rommelrico/filesystem/commands/Touch.scala
@@ -0,0 +1,7 @@
+package com.rommelrico.filesystem.commands
+import com.rommelrico.filesystem.State
+import com.rommelrico.filesystem.files.{DirEntry, File}
+
+class Touch(name: String) extends CreateEntry(name){
+  override def createSpecificEntry(state: State, name: String): DirEntry = File.empty(state.wd.path, name)
+}
