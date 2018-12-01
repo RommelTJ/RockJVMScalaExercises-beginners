c00bb2b5bd47c2194c963dc17424927648087bdb
Rommel Rico <me@rommelrico.com>
2018-11-30 22:28:44 -0800

    Fixing the path in the DirEntry

--- src/com/rommelrico/filesystem/files/DirEntry.scala
+++ src/com/rommelrico/filesystem/files/DirEntry.scala
@@ -1,7 +1,16 @@
 package com.rommelrico.filesystem.files
 
 abstract class DirEntry(val parentPath: String, val name: String) {
-  def path: String = parentPath + Directory.SEPARATOR + name
+
+  def path: String = {
+    val separatorIfNecessary = {
+      if (Directory.ROOT_PATH.equals(parentPath)) ""
+      else Directory.SEPARATOR
+    }
+
+    parentPath + separatorIfNecessary + name
+  }
+
   def asDirectory: Directory
   def asFile: File
   def isDirectory: Boolean
