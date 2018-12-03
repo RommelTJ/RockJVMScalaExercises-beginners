e275afe7fff7a8d775fcd34bce98b123e134bd4c
Rommel Rico <me@rommelrico.com>
2018-12-03 07:16:29 -0800

    Using a curried formatter

--- src/lectures/part3fp/HOFsAndCurries.scala
+++ src/lectures/part3fp/HOFsAndCurries.scala
@@ -35,4 +35,9 @@ object HOFsAndCurries extends App {
   // Functions with multiple parameter lists
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
 
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
 }
