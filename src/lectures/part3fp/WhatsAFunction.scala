6eb12f51e04bcc9071341501fce6c99eda38aa77
Rommel Rico <me@rommelrico.com>
2018-12-02 20:25:51 -0800

    Using MyFunction

--- src/lectures/part3fp/WhatsAFunction.scala
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -3,6 +3,10 @@ package lectures.part3fp
 object WhatsAFunction extends App {
   // Dream: Use a functions as first class elements.
   // Problem: We come from an OOP world.
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
 }
 
 trait MyFunction[A, B] {
