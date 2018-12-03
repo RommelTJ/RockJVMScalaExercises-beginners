7b5998420f038a8357fb08a5af4e184e873ec064
Rommel Rico <me@rommelrico.com>
2018-12-03 07:01:40 -0800

    Higher-Order Function examples

--- src/lectures/part3fp/HOFsAndCurries.scala
+++ src/lectures/part3fp/HOFsAndCurries.scala
@@ -3,4 +3,8 @@ package lectures.part3fp
 object HOFsAndCurries extends App {
   // A function that takes a function as a parameter and returns a function as a result.
   val superiorFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = ???
+
+  // Higher-Order Function (HOF)
+  // Ex: map, flatMap, filter in MyList
+
 }
