972647ea248455ddf1df286b94a701ab7f7f92db
Rommel Rico <me@rommelrico.com>
2018-12-02 20:30:40 -0800

    By Default, Scala supports function like the above up to 22 parameters

--- src/lectures/part3fp/WhatsAFunction.scala
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -15,6 +15,12 @@ object WhatsAFunction extends App {
   }
   println(stringToIntConverter("2") + 3)
 
+  // By Default, Scala supports function like the above up to 22 parameters
+  val adder = new Function2[Int, Int, Int] {
+    override def apply(v1: Int, v2: Int): Int = v1 + v2
+  }
+  println(adder(3, 3))
+
 }
 
 trait MyFunction[A, B] {
