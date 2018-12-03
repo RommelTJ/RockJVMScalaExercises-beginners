29170563aad98d7bdbfd0a5032496465d00914fa
Rommel Rico <me@rommelrico.com>
2018-12-02 20:23:34 -0800

    Action class

--- src/lectures/part3fp/WhatsAFunction.scala
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -4,3 +4,7 @@ object WhatsAFunction extends App {
   // Dream: Use a functions as first class elements.
   // Problem: We come from an OOP world.
 }
+
+class Action {
+  def execute(element: Int): String = ???
+}
