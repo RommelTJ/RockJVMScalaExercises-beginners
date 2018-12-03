3789cbad2f5c1bbf6e4127af7ec00ec1f98dbfd0
Rommel Rico <me@rommelrico.com>
2018-12-02 21:04:24 -0800

    More synctatic sugar

--- src/lectures/part3fp/AnonymousFunctions.scala
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -1,6 +1,8 @@
 package lectures.part3fp
 
 object AnonymousFunctions extends App {
+
   // Anonymous Function (Lambda)
-  val doubler = (x: Int) => x * 2 // Syntactic Sugar for Function1
+  val doubler: Int => Int = x => x * 2 // Syntactic Sugar for Function1
+
 }
