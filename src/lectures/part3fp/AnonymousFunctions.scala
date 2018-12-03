3de9389d33c91f9d153906a15e8b5e38c3dace35
Rommel Rico <me@rommelrico.com>
2018-12-02 21:06:27 -0800

    Multiple params in lambda must be grouped by parenthesis

--- src/lectures/part3fp/AnonymousFunctions.scala
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -5,4 +5,8 @@ object AnonymousFunctions extends App {
   // Anonymous Function (Lambda)
   val doubler: Int => Int = x => x * 2 // Syntactic Sugar for Function1
 
+  // Multiple parameters in a lambda.
+  val adder = (a: Int, b: Int) => a + b
+  val adder2: (Int, Int) => Int = (a, b) => a + b
+
 }
