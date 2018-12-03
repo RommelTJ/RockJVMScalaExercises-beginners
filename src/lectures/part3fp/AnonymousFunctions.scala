b51479721bcd891edeab9fda3264aa4acfb278b8
Rommel Rico <me@rommelrico.com>
2018-12-02 21:09:12 -0800

    No parameter syntax and examples

--- src/lectures/part3fp/AnonymousFunctions.scala
+++ src/lectures/part3fp/AnonymousFunctions.scala
@@ -9,4 +9,10 @@ object AnonymousFunctions extends App {
   val adder = (a: Int, b: Int) => a + b
   val adder2: (Int, Int) => Int = (a, b) => a + b
 
+  // No params
+  val justDoSomething = () => 3
+  val justDoSomething2: () => Int = () => 3
+  println(justDoSomething) // prints lambda (lectures.part3fp.AnonymousFunctions$$$Lambda$9/914504136@2d6e8792)
+  println(justDoSomething()) // prints 3
+
 }
