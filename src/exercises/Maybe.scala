71eb193b030c4e2150aab19e72e8be3b05f20d18
Rommel Rico <me@rommelrico.com>
2018-12-04 07:16:02 -0800

    map, flatMap and filter in Maybe abstract class

--- src/exercises/Maybe.scala
+++ src/exercises/Maybe.scala
@@ -2,7 +2,9 @@ package exercises
 
 // A small collection of at most ONE element (Maybe[+T])
 abstract class Maybe[+T] {
-
+  def map[B](f: T => B): Maybe[B]
+  def flatMap[B](f: T => Maybe[B]): Maybe[B]
+  def filter(p: T => Boolean): Maybe[T]
 }
 
 case object MaybeNot extends Maybe[Nothing] {
