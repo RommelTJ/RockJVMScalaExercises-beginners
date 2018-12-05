060b37739d5a25ca102ab17220b589e57586d6f5
Rommel Rico <me@rommelrico.com>
2018-12-04 19:33:05 -0800

    Using Sequences

--- src/lectures/part3fp/Sequences.scala
+++ src/lectures/part3fp/Sequences.scala
@@ -8,5 +8,10 @@ object Sequences extends App {
   // - Support: apply, iterator, length, reverse, concatenation, appending, preprending, grouping, etc.
   val aSequence = Seq(1, 2, 3, 4)
   println(aSequence) // prints List(1, 2, 3, 4)
+  println(aSequence.reverse)
+  println(aSequence(2)) // prints 3
+  println(aSequence ++ Seq(7, 6, 5)) // Concatenating Sequences
+  val newSequence = aSequence ++ Seq(7, 6, 5)
+  println(newSequence.sorted)
 
 }
