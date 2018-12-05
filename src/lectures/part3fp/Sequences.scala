50e6b744b1a6a1d840d7f61ed330b2a6a4c5f2bf
Rommel Rico <me@rommelrico.com>
2018-12-04 19:30:58 -0800

    Sequences

--- src/lectures/part3fp/Sequences.scala
+++ src/lectures/part3fp/Sequences.scala
@@ -2,6 +2,11 @@ package lectures.part3fp
 
 object Sequences extends App {
 
-
+  // Sequences - A very general interface for data structures that
+  // - have a well defined order
+  // - can be indexed
+  // - Support: apply, iterator, length, reverse, concatenation, appending, preprending, grouping, etc.
+  val aSequence = Seq(1, 2, 3, 4)
+  println(aSequence) // prints List(1, 2, 3, 4)
 
 }
