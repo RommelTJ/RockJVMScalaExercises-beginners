67785af5ca31d1e0513a3eedb93aa2767bacdb7e
Rommel Rico <me@rommelrico.com>
2018-12-05 07:01:40 -0800

    Tuples

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -2,4 +2,7 @@ package lectures.part3fp
 
 object TuplesAndMaps extends App {
 
+  // Tuples - finite ordered "lists"
+  val aTuple = Tuple2(2, "hello, Scala") // Tuple2[Int, String] = (Int, String)
+
 }
