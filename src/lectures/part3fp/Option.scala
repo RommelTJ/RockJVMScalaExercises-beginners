b583916909f55eda3bda8e9f7d9ea9858169cabb
Rommel Rico <me@rommelrico.com>
2018-12-07 09:26:46 -0700

    My First Options

--- src/lectures/part3fp/Option.scala
+++ src/lectures/part3fp/Option.scala
@@ -2,4 +2,9 @@ package lectures.part3fp
 
 object Option extends App {
 
+  val myFirstOption: Option[Int] = Some(4)
+  val noOption: Option[Int] = None
+  println(myFirstOption)
+  println(noOption)
+
 }
