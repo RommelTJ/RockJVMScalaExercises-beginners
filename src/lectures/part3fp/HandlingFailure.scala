0a2e29b28d88e010688b7247088f86ef709398ad
Rommel Rico <me@rommelrico.com>
2018-12-08 09:51:20 -0700

    Create success and failure explicitly

--- src/lectures/part3fp/HandlingFailure.scala
+++ src/lectures/part3fp/HandlingFailure.scala
@@ -1,5 +1,13 @@
 package lectures.part3fp
 
+import scala.util.{Failure, Success}
+
 object HandlingFailure extends App {
+
+  // Create success and failure explicitly
+  val aSuccess = Success(3)
+  val aFailure = Failure(new RuntimeException("Fail!"))
+  println(aSuccess)
+  println(aFailure)
 
 }
