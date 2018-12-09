86d811807f36445e77d85db6350a42bc93051764
Rommel Rico <me@rommelrico.com>
2018-12-09 09:04:19 -0700

    Switch on Steroids

--- src/lectures/part4pm/PatternMatching.scala
+++ src/lectures/part4pm/PatternMatching.scala
@@ -1,5 +1,19 @@
 package lectures.part4pm
 
+import scala.util.Random
+
 object PatternMatching extends App {
+
+  // Switch on Steroids
+  val random = new Random
+  val x = random.nextInt(10)
+
+  val description = x match {
+    case 1 => "the ONE"
+    case 2 => "Double or nothing"
+    case 3 => "Third's time the charm"
+    case _ => "Something else" // _ = WILDCARD
+  }
+  println(s"$x: $description")
 
 }
