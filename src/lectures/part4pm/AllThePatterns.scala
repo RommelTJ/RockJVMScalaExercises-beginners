eb24658a6f1e3dcab06cefc0bcc47134a4049ca0
Rommel Rico <me@rommelrico.com>
2018-12-10 18:07:09 -0700

    Constants with patterns

--- src/lectures/part4pm/AllThePatterns.scala
+++ src/lectures/part4pm/AllThePatterns.scala
@@ -2,6 +2,13 @@ package lectures.part4pm
 
 object AllThePatterns extends App {
 
-
+  // 1 - Constants
+  val x: Any = "Scala"
+  val constants = x match {
+    case 1 => "a numbers"
+    case "scala" => "the scala"
+    case true => "The Truth"
+    case AllThePatterns => "A singleton Object"
+  }
 
 }
