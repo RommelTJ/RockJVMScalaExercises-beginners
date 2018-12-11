03d9337f3904d3b6d0584e23890356df9be18533
Rommel Rico <me@rommelrico.com>
2018-12-11 10:11:52 -0700

    Catches are actually matches

--- src/lectures/part4pm/PatternsEverywhere.scala
+++ src/lectures/part4pm/PatternsEverywhere.scala
@@ -2,4 +2,14 @@ package lectures.part4pm
 
 object PatternsEverywhere extends App {
 
+  // Big Idea #1
+  try {
+    // Some code
+  } catch {
+    case e: RuntimeException => "runtime"
+    case npe: NullPointerException => "NPE"
+    case _ => "Something else"
+  }
+  // Catches are actually matches!
+
 }
